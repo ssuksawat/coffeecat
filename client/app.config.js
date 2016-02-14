c36618770f8d7d8deec4ced8d0e05c4ad06cc0ea
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-02-14 18:24:22 -0500

    add default app theme

--- client/app.config.js
+++ client/app.config.js
@@ -1,9 +1,17 @@
 'use strict';
 
 module.exports = function (app) {
-  app.config(['$logProvider', AppConfig]);
+  app
+    .config(['$logProvider', AppConfig])
+    .config(['$mdThemingProvider', AppTheme]);
 
   function AppConfig($logProvider) {
     $logProvider.debugEnabled(true);
+  }
+
+  function AppTheme($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('cyan')
+      .accentPalette('orange');
   }
 };
