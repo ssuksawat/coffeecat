21dd1a3bf273dc068cf57585c80eb132f07fad8e
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-02-14 16:07:22 -0500

    add cupsize

--- server/models/coffee.model.js
+++ server/models/coffee.model.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const coffeeSchema = mongoose.Schema({
   name: { type: String, required: '{PATH} is required!', unique: true },
   description: { type: String },
+  cupSize: { type: String, enum: ['S', 'M', 'L', 'XL'] },
   ingredients: { type: Array, required: '{PATH} is required!}' },
   feelings: { type: Array, required: '{PATH} is required!' }
 });
