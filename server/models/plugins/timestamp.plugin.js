11ad65830c9e067878d9c0a6c426e0ac72e275f6
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-02-17 16:47:14 -0500

    add timestamp update hook

--- server/models/plugins/timestamp.plugin.js
+++ server/models/plugins/timestamp.plugin.js
@@ -2,10 +2,17 @@ module.exports = function TimestampPlugin(schema) {
   schema.add({ createdAt: Date });
   schema.add({ updatedAt: Date });
 
+  // Hooks for update middleware
+  schema.pre('findOneAndUpdate', function (next) {
+    this.findOneAndUpdate({}, { updatedAt: Date.now() });
+    next();
+  });
+
   schema.pre('save', function (next) {
     const now = new Date();
     if (!this.createdAt) { this.createdAt = now; }
     this.updatedAt = now;
     next();
   });
-}
+
+};
