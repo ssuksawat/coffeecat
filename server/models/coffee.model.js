1993813a63a699bc4fe2039d923805d000bb74f8
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-02-24 01:31:37 -0500

    make ingredients and feelings not required

--- server/models/coffee.model.js
+++ server/models/coffee.model.js
@@ -6,8 +6,8 @@ const coffeeSchema = mongoose.Schema({
   name: { type: String, required: '{PATH} is required!', unique: true },
   description: { type: String },
   cupSize: { type: String, enum: ['S', 'M', 'L', 'XL'] },
-  ingredients: { type: Array, required: '{PATH} is required!}' },
-  feelings: { type: Array, required: '{PATH} is required!' }
+  ingredients: { type: Array },
+  feelings: { type: Array }
 });
 
 coffeeSchema.plugin(require('./plugins/timestamp.plugin'));
