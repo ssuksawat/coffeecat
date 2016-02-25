dbc0d873091e0cec977620688ba6c4abb3452d93
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-02-24 21:26:06 -0500

    add redis url

--- server/config/config.js
+++ server/config/config.js
@@ -7,13 +7,15 @@ module.exports = {
   development: {
     rootPath: rootPath,
     db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/coffeecat',
+    redis: process.env.REDIS_URL || 'redis://localhost:6379',
     port: process.env.PORT || 3000,
     logLevel: process.env.LOG_LEVEL || 'dev',
     secret: process.env.APP_SECRET || 'cool cats love coffee'
   },
   production: {
     rootPath: rootPath,
-    db: process.env.MONGOLAB_URI || process.env.MONGO_URI,
+    db: process.env.MONGOLAB_URI || process.env.MONGO_URL,
+    redis: process.env.REDIS_URL,
     port: process.env.PORT || 80,
     logLevel: process.env.LOG_LEVEL || 'tiny',
     secret: process.env.APP_SECRET || 'cool cats love coffee'
