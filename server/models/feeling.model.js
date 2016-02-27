f69bf356a1c10cbf21e969690bdacb0090d218e7
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-02-27 00:40:10 -0500

    add field 'value'

--- server/models/feeling.model.js
+++ server/models/feeling.model.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const feelingSchema = mongoose.Schema({
   name: { type: String, required: '{PATH} is required!', unique: true },
+  value: { type: Number, min: 0, max: 100 },
   message: { type: String }
 });
 
